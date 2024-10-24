f8bb8e3ceea586d107ebf7f6cc4d15ae22ece45d
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-24 11:54:13 +0530

    Update __main__.py

--- BABYSTRINGHACK/__main__.py
+++ BABYSTRINGHACK/__main__.py
@@ -1,10 +1,17 @@
 import asyncio
 import importlib
-
 from pyrogram import idle
 from BABYSTRINGHACK import LOG
 from BABYSTRINGHACK.modules import ALL_MODULES
+from flask import Flask
+import threading
 
+# Flask app setup
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask app is running on port 8000"
 
 async def start_bot():
     for all_module in ALL_MODULES:
@@ -13,7 +20,14 @@ async def start_bot():
     await idle() 
     LOG.print("[bold red]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
 
-
+def run_flask():
+    # Flask ko 8000 port par run karne ke liye
+    app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
+    # Flask app ko alag thread mein chalana
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    # Bot ko asyncio loop ke saath chalana
     asyncio.get_event_loop().run_until_complete(start_bot())
