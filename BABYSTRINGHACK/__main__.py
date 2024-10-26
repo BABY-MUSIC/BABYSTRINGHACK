688195db11e6f1ba2f755e4e6f0a464b0f43a502
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-26 07:37:43 +0530

    Update __main__.py

--- BABYSTRINGHACK/__main__.py
+++ BABYSTRINGHACK/__main__.py
@@ -4,6 +4,7 @@ from pyrogram import idle
 from BABYSTRINGHACK import LOG
 from BABYSTRINGHACK.modules import ALL_MODULES
 from flask import Flask
+import threading
 
 # Flask app setup
 app = Flask(__name__)
@@ -15,18 +16,18 @@ def home():
 async def start_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("BABYSTRINGHACK.modules." + all_module)
-    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
+    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝗪 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
     await idle() 
     LOG.print("[bold red]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
 
-async def run_flask():
+def run_flask():
     # Flask ko 8000 port par run karne ke liye
-    await asyncio.to_thread(app.run, host="0.0.0.0", port=8000)
-
-async def main():
-    # Flask aur bot ko async tareeke se parallel chalana
-    await asyncio.gather(run_flask(), start_bot())
+    app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    # Main function ko asyncio ke through run karna
-    asyncio.run(main())
+    # Flask app ko alag thread mein chalana
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    # Bot ko asyncio.run ke saath chalana
+    asyncio.run(start_bot())
